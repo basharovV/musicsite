2e51d166f529018a69f1ec02ee911961c36348fa
Slav Basharov <slav@MacBook-Pro.local>
2017-04-13 18:51:53 +0100

    Adding components and initial design

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
+import Header from './components/Header.js';
+import ScrollSection from './components/ScrollSection.js'
 import logo from './logo.svg';
+import Helmet from 'react-helmet'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Helmet>
+          <style>
+            @import url('https://fonts.googleapis.com/css?family=Bahiana');
+            @import url('https://fonts.googleapis.com/css?family=Amatica+SC');
+            @import url('https://fonts.googleapis.com/css?family=Barrio');
+            @import url('https://fonts.googleapis.com/css?family=Dosis');
+            @import url('https://fonts.googleapis.com/css?family=Maven+Pro');
+            @import url('https://fonts.googleapis.com/css?family=Cabin+Sketch');
+            @import url(node_modules/react-blur/dist.css);
+
+
+          </style>
+        </Helmet>
+        <Header/>
+        <ScrollSection/>
       </div>
     );
   }
---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About = () => (
+  <div className="App-about">
+    <h2>About</h2>
+    <p>
+    I compose and produce music for your next creative project!
+    On this website you will find examples of my work
+    in experimental, electronic, jazz, orchestral and ambient styles. 
+    </p>
+  </div>
+)
+
+export default About;
