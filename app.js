e407657b16969764ab820eb5e9b81d74beb8506c
Slav Basharov <slavbacharov@gmail.com>
2017-05-29 18:01:49 +0100

    Allowing all paths in Express

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'build')));
 app.use(express.static('public'))
 
-app.get('/', function (req, res) {
+app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
