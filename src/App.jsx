7acc11d7122df4f9a4c057ffe3326113e906f857
Slav Basharov <slavbacharov@gmail.com>
2017-05-29 23:03:42 +0100

    Added all favicons, document title

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,12 @@ class App extends Component {
             @import url('https://fonts.googleapis.com/css?family=Raleway:200,400,700');
             @import url('https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
           </style>
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+          <link rel="manifest" href="/manifest.json"/>
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>
+          <meta name="theme-color" content="#ffffff"/>
         </Helmet>
         <Header/>
         <Menu/>
