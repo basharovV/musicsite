e9f27a8e6ed2e8a15f7fead33895d452c8361b18
Slav Basharov <slavbacharov@gmail.com>
2017-05-31 22:39:40 +0100

    Added Google Analytics

--- src/index.js
+++ src/index.js
@@ -2,11 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
+import ReactGA from 'react-ga';
 
+ReactGA.initialize('UA-100251861-1');
+
+function logPageView() {
+  ReactGA.set({ page: window.location.pathname + window.location.search });
+  ReactGA.pageview(window.location.pathname + window.location.search);
+}
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter onUpdate={logPageView}>
     <App />
   </BrowserRouter>,
   document.getElementById('root')
