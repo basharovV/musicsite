d74f895c653fa69bd19336e6922b4e6f4df1627f
Slav Basharov <slavbacharov@gmail.com>
2017-06-04 17:20:47 +0100

    ReactGA with react-router v4
    https://github.com/ReactTraining/react-router/issues/4278

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
+import withTracker from './components/withTracker';
+
 import './index.css';
-import { BrowserRouter, createBrowserHistory } from 'react-router-dom';
+import { Route, BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Route component={withTracker(App)}/>
   </BrowserRouter>,
   document.getElementById('root')
 );
